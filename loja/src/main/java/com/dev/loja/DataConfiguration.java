f91fb919c0534ab69835809713fbc1c4424c351f
ArmandoJunior00 <armaanijr00@gmail.com>
2020-10-09 12:28:52 -0300

    Tampletes

---
+++ loja/src/main/java/com/dev/loja/DataConfiguration.java
@@ -0,0 +1,37 @@
+package com.dev.loja;
+
+import javax.sql.DataSource;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.jpa.JpaVendorAdapter;
+import org.springframework.orm.jpa.vendor.Database;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+
+@Configuration
+public class DataConfiguration {
+
+    @Bean
+    public DataSource DataSource(){
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName("com.mysql.jdbc.Driver");
+        dataSource.setUrl("jdbc:mysql://localhost:3307/loja?useTimezone=true&serverTimezone=America/Sao_Paulo");
+        dataSource.setUsername("root");
+        dataSource.setPassword("Pr0d3p@#");
+        return dataSource;
+
+    }
+    
+    @Bean
+    public JpaVendorAdapter jpaVendorAdapter(){
+        HibernateJpaVendorAdapter adapter = new HibernateJpaVendorAdapter();
+        adapter.setDatabase(Database.MYSQL);
+        adapter.setShowSql(true);
+        adapter.setGenerateDdl(true);
+        adapter.setDatabasePlatform("org.hibernate.dialect.MySQL8Dialect");
+        return adapter;
+
+
+    }
+}
